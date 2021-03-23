8f311d9b731d68f7a81a99a9d89a8dc2a3867886
Çağatay Özdemir <cagatay.ozdemir@momentsoft.com>
2021-03-23 21:46:46 +0300

    guncelle

---
+++ EducationSaas/Core/DataAccess/IEntityRepository.cs
@@ -0,0 +1,26 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.DataAccess
+{
+    /// <summary>
+    /// </summary>
+    /// <typeparam name="T">
+    /// t refarans tipinde olmali Ientityden 
+    /// implemente edilis newlenebilir 
+    /// her sey gonderilebilir buraya</typeparam>
+    public interface IEntityRepository<T> where T : class, IEntity, new()
+    {
+        T Get(Expression<Func<T, bool>> filter);
+        List<T> GetList(Expression<Func<T, bool>> filter = null);
+        void Add(T entity);
+        void update(T entity);
+        void Delete(T entity);
+
+    }
+}
---
+++ EducationSaas/DataAccess/Concrete/EntityFrameWork/Context/EduManageDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.EntityFrameWork
+{
+    public class EduManageDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(connectionString: @"");
+        }
+
+        public DbSet<Company> Company { get; set; }
+    }
+}
