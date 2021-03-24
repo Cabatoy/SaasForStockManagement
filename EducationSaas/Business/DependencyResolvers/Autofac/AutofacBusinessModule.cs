26c04e2983025c99bf1d33421a320cd68973749d
Çağatay Özdemir <cagatay.ozdemir@momentsoft.com>
2021-03-24 22:35:40 +0300

    apicilik

---
+++ EducationSaas/Business/DependencyResolvers/Autofac/AutofacBusinessModule.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autofac;
+using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Concrete.EntityFrameWork;
+
+namespace Business.DependencyResolvers.Autofac
+{
+    public class AutofacBusinessModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            //
+            builder.RegisterType<CompanyManager>().As<ICompanyService>();
+            builder.RegisterType<EfCompanyDal>().As<EfCompanyDal>();
+        }
+    }
+}
