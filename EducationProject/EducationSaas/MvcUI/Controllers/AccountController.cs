5fda20df68f1da50f3d8884f609459f0e4a80b4d
Berat Özmen <bzmen@hotmail.com>
2021-03-28 11:49:18 +0300

    login metodu için api tarafında getall metoduna erişim sağlandı

--- EducationProject/EducationSaas/MvcUI/Controllers/AccountController.cs
+++ EducationProject/EducationSaas/MvcUI/Controllers/AccountController.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Web;
 using System.Web.Mvc;
 
@@ -8,11 +11,33 @@ namespace MvcUI.Controllers
 {
     public class AccountController : Controller
     {
-       public ActionResult Login()
+        public ActionResult Login()
         {
+            var httpHandler = new HttpClientHandler();
+            httpHandler.ServerCertificateCustomValidationCallback = (message, cert, chain, sslPolicyErrors) =>
+            {
+                return true;
+            };
+            using (var client = new HttpClient(httpHandler))
+            {
+                //ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12 | SecurityProtocolType.Tls11;
+
+                client.BaseAddress = new Uri("https://localhost:44329/");
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                HttpResponseMessage response;
+                response = client.GetAsync("api/users/getall").Result;
+                if (response.IsSuccessStatusCode)
+                {
+                }
+                else
+                {
+                }
+            }
+
             return View();
         }
-        
+
         public ActionResult LogOut()
         {
             return View();
