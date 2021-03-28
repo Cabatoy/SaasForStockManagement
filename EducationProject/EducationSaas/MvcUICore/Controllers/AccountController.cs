14cc886bc93d65678b1297c32728227b375cb6ee
Berat Özmen <bzmen@hotmail.com>
2021-03-28 18:17:57 +0300

    core' a taşınmaca

---
+++ EducationProject/EducationSaas/MvcUICore/Controllers/AccountController.cs
@@ -0,0 +1,26 @@
+﻿using Entities.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using MvcUICore.Utility.Api;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcUICore.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Login()
+        {
+            Users user = new Users{ Id=1,FullName="berat",CompanyId=1 };
+
+            HttpService.Post("users", "add",user);
+
+            HttpService.Get("users", "getById", 5);
+
+            HttpService.Get("users", "getall");
+
+            return View();
+        }
+    }
+}
--- EducationProject/EducationSaas/MvcUICore/Utility/Api/HttpService.cs
+++ EducationProject/EducationSaas/MvcUICore/Utility/Api/HttpService.cs
@@ -1,17 +1,17 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Headers;
+using System.Text;
 using System.Web;
 
-namespace MvcUI.Utility.Api
+namespace MvcUICore.Utility.Api
 {
     public class HttpService
     {
         static Uri url = new Uri("https://localhost:44329/api");
-
-       
 
         public static string Get(string controllerName, string requestUri)
         {
@@ -51,8 +51,38 @@ namespace MvcUI.Utility.Api
             using (var client = new HttpClient(httpHandler))
             {
                 client.BaseAddress = new Uri(url.AbsoluteUri + "/" + controllerName + "/");
+                client.DefaultRequestHeaders.Accept.Clear();
                 client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
                 HttpResponseMessage response = client.GetAsync(requestUri + "/" + id.ToString()).Result;
+                if (response.IsSuccessStatusCode)
+                {
+                    //if (response.Content.Headers.ContentLength <= 4)
+                    //    result = string.Empty;
+                    //else
+                    //    result = response.Content.ReadAsAsync<object>().Result.ToString();
+                }
+            }
+            return result;
+        }
+
+
+        public static string Post(string controllerName, string requestUri, object model)
+        {
+            var httpHandler = new HttpClientHandler();
+            httpHandler.ServerCertificateCustomValidationCallback = (message, cert, chain, sslPolicyErrors) =>
+            {
+                return true;
+            };
+
+            string result = string.Empty;
+            using (var client = new HttpClient(httpHandler))
+            {
+                client.BaseAddress = new Uri(url.AbsoluteUri + "/" + controllerName + "/");
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                var content = new StringContent(JsonConvert.SerializeObject(model), Encoding.UTF8, "application/json");
+                content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+                HttpResponseMessage response = client.PostAsync(requestUri, content).Result;
                 if (response.IsSuccessStatusCode)
                 {
                     //if (response.Content.Headers.ContentLength <= 4)
